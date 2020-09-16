e10fab087f43c580562863ea02881fc53bae85df
bartolomejkozorog <bartolomej.kozorog@gmail.com>
2020-09-16 23:49:18 +0200

    fix env validation + init

--- server/src/env.ts
+++ server/src/env.ts
@@ -1,6 +1,9 @@
 const envalid = require('envalid');
 const { str, num } = envalid;
 const { join } = require('path');
+const { EnvMissingError } = require('envalid');
+const chalk = require('chalk');
+const RULE = chalk.grey('================================')
 
 const dotEnvPath = process.env.NODE_ENV !== 'production'
   ? join(__dirname, '..', '.env.development')
@@ -36,9 +39,38 @@ export const workerEnv = () => envalid.cleanEnv(process.env, {
   reporter
 });
 
+// same as default reporter but doesn't exit the process
+// code taken from https://github.com/af/envalid/blob/master/src/reporter.js
 function reporter ({ errors, env }) {
-  console.warn(`Some env variables may be configured incorrectly!`)
-  errors.forEach(err => {
-    console.error(err.message)
-  });
+  const errorKeys = Object.keys(errors)
+  if (!errorKeys.length) return
+
+  const missingVarsOutput = []
+  const invalidVarsOutput = []
+  for (const k of errorKeys) {
+    const err = errors[k]
+    if (err instanceof EnvMissingError) {
+      missingVarsOutput.push(`    ${chalk.blue(k)}: ${errors[k].message || '(required)'}`)
+    } else invalidVarsOutput.push(`    ${chalk.blue(k)}: ${errors[k].message}`)
+  }
+
+  // Prepend "header" output for each section of the output:
+  if (invalidVarsOutput.length) {
+    invalidVarsOutput.unshift(` ${chalk.yellow('Invalid')} environment variables:`)
+  }
+  if (missingVarsOutput.length) {
+    missingVarsOutput.unshift(` ${chalk.yellow('Missing')} environment variables:`)
+  }
+
+  const output = [
+    RULE,
+    invalidVarsOutput.join('\n'),
+    missingVarsOutput.join('\n'),
+    chalk.yellow('\n Exiting with error code 1'),
+    RULE
+  ]
+    .filter(x => !!x)
+    .join('\n')
+
+  console.error(output);
 }
